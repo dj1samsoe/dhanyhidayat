c9084575c11262b924c7ad10ea25d6cda9e83aaf
dj1samsoe <dhanyh86@gmail.com>
2023-09-05 16:11:56 +0700

    feat (add PageSpeed)

--- common/constant/index.ts
+++ common/constant/index.ts
@@ -1,8 +1,7 @@
 export const PLACEHOLDER_URL =
   'https://res.cloudinary.com/dvlbwm8c1/image/upload/v1692937492/codebayu/o2c0ezrx8nxaqbok2qdr.png';
 export const DEVTO_PROFILE = 'https://dev.to/dj1samsoe';
-export const PAGESPEED_URL =
-  'https://pagespeed.web.dev/analysis/https-dhanyhidayat-vercel-app/23438htyga?form_factor=desktop';
+export const PAGESPEED_URL = 'https://pagespeed.web.dev/';
 export const PAGESPEED_CATEGORIES = '&category=accessibility&category=performance&category=best-practices&category=seo';
 export const PROFILE_URL =
   'https://res.cloudinary.com/dfcwcfx5z/image/upload/v1693769407/djisamsoe/tenvc6lnt4zuazowsbu7.jpg';
