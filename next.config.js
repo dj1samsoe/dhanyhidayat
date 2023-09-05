7eded40fd42522cce455776fa220585def43b83b
dj1samsoe <dhanyh86@gmail.com>
2023-09-05 22:03:03 +0700

    feat (add Google analytics)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['picsum.photos', 'res.cloudinary.com'],
-  },
+    domains: ['picsum.photos', 'res.cloudinary.com']
+  }
 };
-
 module.exports = nextConfig;
+
+const withGA = require('@bradgarropy/next-google-analytics');
+
+module.exports = withGA({
+  googleAnalytics: {
+    id: 'G-20W6BJQQVJ' // Ganti dengan kode pelacakan Anda
+  }
+});
