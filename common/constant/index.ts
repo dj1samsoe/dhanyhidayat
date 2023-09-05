6da08c1e003967e3e2b9c2af44ea7aff7e45b733
dj1samsoe <dhanyh86@gmail.com>
2023-09-05 16:16:47 +0700

    feat (add PageSpeed)

--- common/constant/index.ts
+++ common/constant/index.ts
@@ -2,6 +2,7 @@ export const PLACEHOLDER_URL =
   'https://res.cloudinary.com/dvlbwm8c1/image/upload/v1692937492/codebayu/o2c0ezrx8nxaqbok2qdr.png';
 export const DEVTO_PROFILE = 'https://dev.to/dj1samsoe';
 export const PAGESPEED_URL = 'https://pagespeed.web.dev/';
+export const NEXT_PUBLIC_PAGE_SPEED_API = process.env.NEXT_PUBLIC_PAGE_SPEED_API;
 export const PAGESPEED_CATEGORIES = '&category=accessibility&category=performance&category=best-practices&category=seo';
 export const PROFILE_URL =
   'https://res.cloudinary.com/dfcwcfx5z/image/upload/v1693769407/djisamsoe/tenvc6lnt4zuazowsbu7.jpg';
