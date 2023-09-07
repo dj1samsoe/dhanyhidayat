83f8d90497bec2e1f662a410e5ca875760444295
dj1samsoe <dhanyh86@gmail.com>
2023-09-07 15:09:32 +0700

    feat (add responsive saweria button)

--- modules/me/components/GoHome.tsx
+++ modules/me/components/GoHome.tsx
@@ -8,13 +8,16 @@ import { SAWERIA_URL } from '@/common/constant';
 
 export default function GoHome() {
   return (
-    <div className="flex w-full capitalize justify-center mt-8" data-aos="zoom-in-down">
+    <div
+      className="flex flex-col space-y-3 md:space-y-0 md:space-x-3 px-6 md:flex-row w-full justify-center mt-8"
+      data-aos="zoom-in-down"
+    >
       <Link
         href="/"
-        className="flex border w-max shadow-sm m-2 border-neutral-200 items-center px-6 py-2 rounded-xl gap-2 hover:gap-3 delay-75 hover:transition-all hover:duration-300 transition-all duration-300"
+        className="flex border w-full h-max md:w-max shadow-sm border-neutral-200 dark:border-neutral-700 justify-center items-center px-6 py-3 rounded-xl gap-2 hover:gap-3 delay-75 hover:transition-all hover:duration-300 transition-all duration-300"
       >
         <FcMindMap />
-        <span>Awesome magic</span>
+        <span>Awesome Magic</span>
       </Link>
       <Link
         href={SAWERIA_URL}
