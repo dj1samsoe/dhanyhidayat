b5cc127919a4b68a477e9db730258f12d75565ef
dj1samsoe <dhanyh86@gmail.com>
2023-09-07 14:45:53 +0700

    feat (add Saweria)

--- modules/home/components/Introduction.tsx
+++ modules/home/components/Introduction.tsx
@@ -7,12 +7,14 @@ import { SAWERIA_URL } from '@/common/constant';
 export default function Introduction() {
   return (
     <section className="bg-cover bg-no-repeat space-y-2">
-      <div className="flex gap-2 text-2xl lg:text-3xl font-medium font-sora">
-        <h1>Hi, I&apos;m Dhany</h1> <div className="ml-1 animate-waving-hand">👋</div>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 text-2xl lg:text-3xl font-medium font-sora">
+          <h1>Hi, I&apos;m Dhany</h1> <div className="ml-1 animate-waving-hand">👋</div>
+        </div>
+        <Link href={SAWERIA_URL} target="_blank" className="">
+          <TbCoffee size={25} />
+        </Link>
       </div>
-      <Link href={SAWERIA_URL} target="_blank" className="">
-        <TbCoffee size={25} />
-      </Link>
 
       <div className="space-y-4">
         <ul className="flex flex-col lg:flex-row gap-1 lg:gap-8 ml-5 list-disc text-neutral-700 dark:text-neutral-400">
