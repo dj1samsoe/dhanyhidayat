7cc167a96b22d83235db8f1c02e3cd8e9e0f3142
dj1samsoe <dhanyh86@gmail.com>
2023-09-13 19:34:31 +0700

    change style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,13 +11,13 @@ export default function NotFound() {
         data-aos="fade-up"
         className="flex flex-col text-neutral-600 dark:text-neutral-300 space-y-10 items-center justify-center lg:h-[90vh] overflow-hidden"
       >
-        <h1 className="text-4xl md:text-6xl font-semibold">Why am i here?</h1>
+        <h1 className="text-4xl md:text-6xl font-semibold">Where are you going to?</h1>
         <p className="text-center">It seems like the page that you are looking for is no longer here.</p>
         <Link
           href="/"
           className="uppercase text-xs px-6 py-4 rounded-full dark:bg-neutral-300 bg-neutral-600 text-neutral-100 dark:text-neutral-700 hover:shadow-lg transition-all duration-300"
         >
-          Take me home
+          Go to Home
         </Link>
       </Container>
     </>
--- modules/dashboard/components/Dashboard.tsx
+++ modules/dashboard/components/Dashboard.tsx
@@ -1,6 +1,5 @@
 import Breakline from '@/common/components/elements/Breakline';
 
-import CodingActive from './CodingActive';
 import Contributions from './Contributions';
 
 interface DashboardProps {
@@ -9,8 +8,6 @@ interface DashboardProps {
 export default function Dashboard({ githubData }: DashboardProps) {
   return (
     <section className="flex flex-col">
-      <CodingActive />
-      <Breakline />
       <Contributions githubData={githubData} />
     </section>
   );
