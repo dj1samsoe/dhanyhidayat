5c2aeb0fdcb1af53c02644d013c221db9a606cd5
dj1samsoe <dhanyh86@gmail.com>
2024-01-13 16:43:10 +0700

    feat(about): add resume

--- modules/about/components/About.tsx
+++ modules/about/components/About.tsx
@@ -2,12 +2,17 @@ import Breakline from '@/common/components/elements/Breakline';
 
 import CareerList from './CareerList';
 import EducationList from './EducationList';
+import Resume from './Resume';
+import SkillList from './SkillList';
 import Summary from './Summary';
 
 export default function About() {
   return (
     <section className="flex flex-col">
       <Summary />
+      <Resume />
+      <Breakline className="my-8" />
+      <SkillList />
       <Breakline className="my-8" />
       <CareerList />
       <Breakline className="my-8" />
