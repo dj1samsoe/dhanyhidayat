778114a86837fe7fbfcd3d2ce9f3835489d9bbca
dj1samsoe <dhanyh86@gmail.com>
2024-01-20 23:44:57 +0700

    refactor(useServer): remove use server

--- common/libs/spotify.ts
+++ common/libs/spotify.ts
@@ -1,27 +1,3 @@
-'use server';
-
-// import querystring from 'querystring';
-
-// const client_id = process.env.NEXT_PUBLIC_CLIENT_ID_SPOTIFY;
-// const client_secret = process.env.NEXT_PUBLIC_CLIENT_SECRET_SPOTIFY;
-// const refresh_token = process.env.NEXT_PUBLIC_REFRESH_TOKEN_SPOTIFY;
-// const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
-// const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
-// export const getAccessToken = async () => {
-//   const response = await fetch(TOKEN_ENDPOINT, {
-//     method: 'POST',
-//     headers: {
-//       Authorization: `Basic ${basic}`,
-//       'Content-Type': 'application/x-www-form-urlencoded'
-//     },
-//     body: querystring.stringify({
-//       grant_type: 'refresh_token',
-//       refresh_token
-//     })
-//   });
-//   return response.json();
-// };
-
 export const getAccessToken = async () => {
   const myHeaders = new Headers();
   myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   images: {
     domains: ['picsum.photos', 'res.cloudinary.com', 'i.scdn.co', 'media.dev.to']
-  },
-  experimental: {
-    serverComponents: true,
-    serverActions: true
   }
 };
 module.exports = nextConfig;
