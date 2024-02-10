96008cee1bbc39d7e8283a5059c6e75d9405df62
dj1samsoe <dhanyh86@gmail.com>
2024-02-11 03:41:33 +0700

    style remove viewOptions Grid for fix bug

--- common/components/elements/LoadingPreviewCard.tsx
+++ common/components/elements/LoadingPreviewCard.tsx
@@ -12,7 +12,7 @@ export default function LoadingPreviewCard({ view }: { view: string }) {
     isMobile ? setViewOption('grid') : setViewOption(view);
   }, [isMobile, view]);
   return (
-    <div className={clsxm('w-full flex overflow-hidden', viewOption === 'grid' ? 'flex-col sm:w-full' : '')}>
+    <div className={clsxm('w-full flex overflow-hidden flex-col sm:w-full')}>
       <div className="animate-pulse bg-neutral-200 dark:bg-neutral-700 h-32 rounded-lg"></div>
       <div className="flex flex-col flex-1 space-y-3 py-3">
         <div className="h-3 animate-pulse bg-neutral-200 dark:bg-neutral-700 rounded"></div>
