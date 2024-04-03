fc45981264364c6519a7f16a2d7335b57a8a028e
dj1samsoe <dhanyh86@gmail.com>
2024-04-03 20:38:48 +0700

    style: change layout for education

--- common/types/education.ts
+++ common/types/education.ts
@@ -8,3 +8,15 @@ export interface EducationProps {
   end_year: number;
   link: string;
 }
+
+export interface CertificateProps {
+  position: string;
+  company: string;
+  logo: string | null;
+  location: string;
+  location_type: string;
+  type: string;
+  start_date: string;
+  end_date: string | null;
+  link: string | null;
+}
