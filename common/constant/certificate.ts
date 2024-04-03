1d10e58f0793f4b5d4fcb2eeb5c6c53425a2a0ce
dj1samsoe <dhanyh86@gmail.com>
2024-04-03 20:38:17 +0700

    feat: add new element for certification list

---
+++ common/constant/certificate.ts
@@ -0,0 +1,46 @@
+export const CERTIFICATES = [
+  {
+    position: 'Belajar Dasar Pemrograman JavaScript',
+    company: 'Dicoding Indonesia',
+    logo: 'https://res.cloudinary.com/dfcwcfx5z/image/upload/v1693915680/djisamsoe/careers/ldewfprkpiphyuzna12b.webp',
+    location: 'Bandung',
+    location_type: 'Online',
+    type: 'Fulltime',
+    start_date: '2023-08',
+    end_date: '2023-11',
+    link: 'https://www.dicoding.com/certificates/NVP7OVV6WPR0'
+  },
+  {
+    position: 'Frontend - React',
+    company: 'MySkill.id',
+    logo: 'https://res.cloudinary.com/dfcwcfx5z/image/upload/v1693915681/djisamsoe/careers/unqjqlh4slklqgo4zvb0.png',
+    location: 'Jakarta',
+    location_type: 'Online',
+    type: 'Fulltime',
+    start_date: '2023-08',
+    end_date: '2023-09',
+    link: 'https://storage.googleapis.com/myskill-v2-certificates/topic-sTLaN1dknfdCpAbUm5O2/vRPmxR4xeeSd2g7J55QRDc4TEG52-u4HlSOKomPKeY4GFqwag.pdf'
+  },
+  {
+    position: 'Basic HTML & CSS',
+    company: 'Dibimbing.id',
+    logo: 'https://res.cloudinary.com/dfcwcfx5z/image/upload/v1693915680/djisamsoe/careers/aoxqhhpzaucvw1jzx8e9.png',
+    location: 'Jakarta',
+    location_type: 'Online',
+    type: 'Fulltime',
+    start_date: '2023-07',
+    end_date: '2023-08',
+    link: 'https://dibimbing-lms-dev.s3.ap-southeast-1.amazonaws.com/production/Basic%20HTML%20%26%20CSS/VOD20230803307.pdf'
+  },
+  {
+    position: 'Frontend - JavaScript',
+    company: 'MySkill.id',
+    logo: 'https://res.cloudinary.com/dfcwcfx5z/image/upload/v1693915681/djisamsoe/careers/unqjqlh4slklqgo4zvb0.png',
+    location: 'Jakarta',
+    location_type: 'Online',
+    type: 'Fulltime',
+    start_date: '2023-08',
+    end_date: '2023-09',
+    link: 'https://storage.googleapis.com/myskill-v2-certificates/topic-pTLX6J38RGSQ4dfEO6yQ/vRPmxR4xeeSd2g7J55QRDc4TEG52-XUyEHWrp1eGqKhFjsxwf.pdf'
+  }
+];
