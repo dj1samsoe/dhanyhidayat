cd8aadf50b78034032a6e28737527a019735e81b
dj1samsoe <dhanyh86@gmail.com>
2024-06-10 02:29:54 +0700

    style(service): change service card layout and style

--- modules/home/components/ServicesList.tsx
+++ modules/home/components/ServicesList.tsx
@@ -9,14 +9,14 @@ import ServicesCard from './ServicesCard';
 
 export default function ServicesList() {
   return (
-    <section>
+    <section className="w-full">
       <div className="space-y-2">
         <SectionHeading title="Services" icon={<FaServicestack className="mr-1" />} />
         <SectionSubHeading>
           <p className="dark:text-neutral-400">What can I do for you</p>
         </SectionSubHeading>
       </div>
-      <div className="flex flex-row space-x-3 mt-6 overflow-x-scroll no-scrollbar">
+      <div className="flex flex-row space-x-3 mt-6 overflow-x-scroll no-scrollbar w-full">
         {servicesMock?.map((item, index) => <ServicesCard key={index} {...item} />)}
       </div>
     </section>
