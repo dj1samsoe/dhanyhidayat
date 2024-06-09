a623fdfdbd9b51def8cc571171f8e9f166afe816
dj1samsoe <dhanyh86@gmail.com>
2024-06-10 02:26:59 +0700

    style(loadingPreviewCard): change layout of loading card

--- common/components/elements/LoadingPreviewCard.tsx
+++ common/components/elements/LoadingPreviewCard.tsx
@@ -12,15 +12,8 @@ export default function LoadingPreviewCard({ view }: { view: string }) {
     isMobile ? setViewOption('grid') : setViewOption(view);
   }, [isMobile, view]);
   return (
-    <div className={clsxm('w-full flex overflow-hidden flex-col sm:w-full')}>
-      <div className="animate-pulse bg-neutral-200 dark:bg-neutral-700 h-32 rounded-lg"></div>
-      <div className="flex flex-col flex-1 space-y-3 py-3">
-        <div className="h-3 animate-pulse bg-neutral-200 dark:bg-neutral-700 rounded"></div>
-        <div className="flex space-x-4">
-          <div className={clsxm('h-2 animate-pulse bg-neutral-200 dark:bg-neutral-700 rounded w-20')}></div>
-          <div className={clsxm('h-2 animate-pulse bg-neutral-200 dark:bg-neutral-700 rounded w-20')}></div>
-        </div>
-      </div>
+    <div className={clsxm('w-full overflow-x-hidden sm:w-full')}>
+      <div className="animate-pulse bg-neutral-200 dark:bg-neutral-700 h-[400px] w-[250px] rounded-lg"></div>
     </div>
   );
 }
