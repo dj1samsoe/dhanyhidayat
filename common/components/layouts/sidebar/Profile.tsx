a344cdc94be357fe019f3c970648fa8d091a86d2
dj1samsoe <dhanyh86@gmail.com>
2024-06-11 00:15:58 +0700

    style(Profile): change light background color

--- common/components/layouts/sidebar/Profile.tsx
+++ common/components/layouts/sidebar/Profile.tsx
@@ -35,7 +35,7 @@ export default function Profile() {
   return (
     <div
       className={clsx(
-        'z-20 fixed shadow-sm xl:shadow-none lg:border-none dark:border-b dark:border-neutral-800 bg-neutral-200 dark:bg-dark lg:!bg-transparent w-full p-5 lg:relative lg:p-0',
+        'z-20 fixed shadow-sm xl:shadow-none lg:border-none dark:border-b dark:border-neutral-800 bg-light dark:bg-dark lg:!bg-transparent w-full p-5 lg:relative lg:p-0',
         isOpen && 'pb-0'
       )}
     >
