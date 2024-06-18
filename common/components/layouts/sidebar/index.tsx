445fc49e9c31786394ff400cb0c7f0cd1f6fc31d
dj1samsoe <dhanyh86@gmail.com>
2024-06-19 03:02:45 +0700

    style(Sidebar): remove space-y

--- common/components/layouts/sidebar/index.tsx
+++ common/components/layouts/sidebar/index.tsx
@@ -6,7 +6,7 @@ import Profile from './Profile';
 
 export default function Sidebar() {
   return (
-    <div className="sticky transition-all duration-300 top-0 z-10 flex flex-col space-y-4 lg:py-8 lg:dark:bg-[#4949492e] lg:bg-white lg:px-4 lg:w-64 lg:rounded-lg">
+    <div className="sticky transition-all duration-300 top-0 z-10 flex flex-col lg:py-8 lg:dark:bg-[#4949492e] lg:bg-white lg:px-4 lg:w-64 lg:rounded-lg">
       <Profile />
       <nav className="hidden md:block">
         <Breakline />
