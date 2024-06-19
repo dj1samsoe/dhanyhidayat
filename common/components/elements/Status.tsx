e83476b2fd5d40ea27e3100ee573b0b99b8a5303
dj1samsoe <dhanyh86@gmail.com>
2024-06-19 15:53:25 +0700

    style(Status): add motion effect like ThemeToggle

--- common/components/elements/Status.tsx
+++ common/components/elements/Status.tsx
@@ -4,7 +4,12 @@ import { motion } from 'framer-motion';
 
 export default function Status() {
   return (
-    <div className="flex items-center gap-2 border dark:border-neutral-700 rounded-full px-2 py-1 bg-transparent">
+    <motion.div
+      className="flex items-center gap-2 border dark:border-neutral-700 rounded-full px-2 py-1 bg-transparent"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5, delay: 0.5 }}
+    >
       <motion.div
         data-testid="available-collabs-container"
         className="h-2 w-2 rounded-full bg-green-400"
@@ -12,6 +17,6 @@ export default function Status() {
         transition={{ duration: 0.5, repeat: Infinity }}
       />
       <span className="text-xs text-neutral-600 dark:text-neutral-400">Hire me</span>
-    </div>
+    </motion.div>
   );
 }
