8d167a298fa53b0a3a607e37046b6d6d2d5fe77a
dj1samsoe <dhanyh86@gmail.com>
2025-02-02 00:33:11 +0700

    fix(devToImage): change dev to image url

--- next.config.js
+++ next.config.js
@@ -20,6 +20,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'media2.dev.to'
+      },
+      {
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com'
       }
     ]
