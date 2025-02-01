9033121d6ccdebc5dc7e384a0df9339d2004ed11
dj1samsoe <dhanyh86@gmail.com>
2025-02-02 00:36:01 +0700

    chore(careers): add new career list

--- common/constant/careers.ts
+++ common/constant/careers.ts
@@ -1,6 +1,55 @@
 export const CAREERS = [
   {
-    position: 'Web Developer',
+    position: 'Web Developer Intern',
+    company: 'Cognitia Strategi Indonesia',
+    company_legal_name: 'PT. Cognitia Strategi Indonesia',
+    logo: '/cognitia.webp',
+    location: 'Surabaya, Indonesia 🇮🇩',
+    location_type: 'Hybrid',
+    type: 'Internship',
+    start_date: '2024-12-12',
+    end_date: null,
+    industry: 'wellness',
+    link: 'https://cognitia.id',
+    responsibilities: ['Developed and maintained Online Shop website of Cognitia Foundation using Wordpress']
+  },
+  {
+    position: 'Frontend Developer',
+    company: 'Traction Energy Asia',
+    company_legal_name: 'PT. Traction Energy Asia',
+    logo: '/tea.png',
+    location: 'Indonesia 🇮🇩',
+    location_type: 'Remote',
+    type: 'Freelance',
+    start_date: '2024-07-11',
+    end_date: null,
+    industry: 'wellness',
+    link: 'https://tractionenergy.asia/',
+    responsibilities: [
+      'Developed UI Layout on E-Learning TEA website using Odoo',
+      'Brainstorming about tools that used',
+      'Customize layout in Odoo Module using Tailwind CSS'
+    ]
+  },
+  {
+    position: 'Freelance Web Developer',
+    company: 'projects.co.id',
+    company_legal_name: 'projects.co.id',
+    logo: '/projects.ico',
+    location: 'Indonesia 🇮🇩',
+    location_type: 'Remote',
+    type: 'Freelance',
+    start_date: '2025-01-11',
+    end_date: '2025-01-31',
+    industry: 'wellness',
+    link: 'https://projects.co.id',
+    responsibilities: [
+      'Creating online shop using Wordpress, Woocommerce and Elementor',
+      'Integration with Xendit payment gateway and Ongkoskirim.id plugin for shipping'
+    ]
+  },
+  {
+    position: 'Web Developer Intern',
     company: 'Cognitia Strategi Indonesia',
     company_legal_name: 'PT. Cognitia Strategi Indonesia',
     logo: '/cognitia.webp',
@@ -8,14 +57,17 @@ export const CAREERS = [
     location_type: 'Hybrid',
     type: 'Internship',
     start_date: '2024-07-12',
-    end_date: null,
+    end_date: '2024-10-12',
     industry: 'wellness',
     link: 'https://cognitia.id',
     responsibilities: [
-      'Maintenanced and developed company website using Wordpress',
-      'Redesign company website using Wordpress'
+      'Developed and maintained company website using Wordpress.',
+      'Contributed to weekly meetings (hybrid) with the team.',
+      'Problem-solving based on discussions with the team',
+      'Exploring new knowledge about WordPress plugin'
     ]
   },
+
   {
     position: 'Frontend Developer',
     company: 'Kawan Kerja',
