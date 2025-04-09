4c65c9e0ca3f8608de525b36d42bfbfb5a9c4c72
dj1samsoe <dhanyh86@gmail.com>
2025-04-09 08:39:21 +0700

    style: fix layout when formatted ul and ol component

--- common/components/elements/MDXComponent.tsx
+++ common/components/elements/MDXComponent.tsx
@@ -29,8 +29,8 @@ const MDXComponent = ({ children }: MarkdownRendererProps) => {
         p: props => <div {...props} />,
         h2: props => <h2 className="text-xl font-medium dark:text-neutral-300" {...props} />,
         h3: props => <h3 className="text-[18px] leading-snug pt-4 font-medium dark:text-neutral-300" {...props} />,
-        ul: props => <ul className="pl-10 space-y-3 list-disc pb-5" {...props} />,
-        ol: props => <ol className="pl-10 space-y-3 list-decimal pb-5" {...props} />,
+        ul: props => <ul className="pl-10 space-y-1 list-disc pb-5" {...props} />,
+        ol: props => <ol className="pl-10 space-y-1 list-decimal pb-5" {...props} />,
         code: props => <CodeBlock {...props} />,
         blockquote: props => (
           <blockquote
