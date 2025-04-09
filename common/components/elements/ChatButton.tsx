bd7cd5818546de3ee5ddeb055aaf8c753e66142d
dj1samsoe <dhanyh86@gmail.com>
2025-04-09 08:38:29 +0700

    style: change color when dark mode

--- common/components/elements/ChatButton.tsx
+++ common/components/elements/ChatButton.tsx
@@ -21,10 +21,10 @@ export default function ChatButton() {
     <>
       <button
         aria-label="chat button"
-        className="fixed bottom-6 right-6 p-3 bg-neutral-800 rounded-full hover:bg-neutral-700 transition-all duration-300 shadow-lg"
+        className="fixed bottom-6 right-6 p-3 bg-neutral-800 dark:bg-neutral-300 rounded-full dark:hover:bg-neutral-400 hover:bg-neutral-700 transition-all duration-300 shadow-lg"
         onClick={handleChatButtonClick}
       >
-        <BotIcon size={30} className="text-neutral-300" />
+        <BotIcon size={30} className="text-neutral-300 dark:text-neutral-800" />
       </button>
       <AnimatePresence>{isChatOpen && <Chatbot isOpen={isChatOpen} onClose={handleCloseChat} />}</AnimatePresence>
     </>
